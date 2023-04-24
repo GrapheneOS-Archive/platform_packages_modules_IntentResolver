2bc18fb62994d7c1b9b6dfd57974cd7a62e0cb3e
Matt Casey <mrcasey@google.com>
2023-04-24 15:12:41 +0000

    Revert "Revert^2 "Revert "Move Nearby to the front of the app list."""
    This reverts commit ffc1dc5709c794effc8b2e7b0761a80ecc8abb77.
    
    Reason for revert: Removing the offending portion of the test in ag/22815196
    
    Change-Id: I8787f57e570e7fe2fe8bcb38aef33d14ae694d8d

--- java/src/com/android/intentresolver/contentpreview/ChooserContentPreviewUi.java
+++ java/src/com/android/intentresolver/contentpreview/ChooserContentPreviewUi.java
@@ -66,10 +66,6 @@ public final class ChooserContentPreviewUi {
         @Nullable
         ActionRow.Action createEditButton();
 
-        /** Create an "Share to Nearby" action. */
-        @Nullable
-        ActionRow.Action createNearbyButton();
-
         /** Create custom actions */
         List<ActionRow.Action> createCustomActions();
 
